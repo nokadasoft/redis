e5e1b40a265993f5ad156947edd2c5878ac79069
nokadasoft <nokadasoft@outlook.com>
2018-11-17 00:30:44 +0000

    it works! core api remote

--- redis-core-back/Program.cs
+++ redis-core-back/Program.cs
@@ -19,6 +19,7 @@ namespace redis_core_back
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>()
                 .Build();
     }
