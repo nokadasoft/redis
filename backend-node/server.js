70b3ff3c0092c9f1614b95b147152d688c5cbf09
Norbert Dabrowski <norbert@automated-intelligence.com>
2018-11-20 14:28:54 +0000

    changed port numbers for backend

--- backend-node/server.js
+++ backend-node/server.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
