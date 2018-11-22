78cfc00ea7574472a7d39c80bcacb2e94e9bb1d7
Norbert Dabrowski <norbert@automated-intelligence.com>
2018-11-22 09:25:32 +0000

    added angular frontend to docker compose

--- frontend-angular/src/karma.conf.js
+++ frontend-angular/src/karma.conf.js
@@ -20,6 +20,13 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
