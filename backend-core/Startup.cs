07d3e60665dba826907a8b7a939948acd81e25bb
nokadasoft <nokadasoft@outlook.com>
2018-11-23 22:18:54 +0000

    swagger nuget added

--- backend-core/Startup.cs
+++ backend-core/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace redis_core_back
 {
@@ -38,6 +39,18 @@ namespace redis_core_back
             //     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
             //     options.KnownProxies.Add(IPAddress.Parse("127.0.0.1"));
             // });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "redis backend core API",
+                    Description = "ASP.NET Core Web API fro Redis Backend Core",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Nokadasoft", Email = "nokadasoft@outlook.com", Url = "www.nokadasoft.com" }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,8 +64,7 @@ namespace redis_core_back
 
 
             // https://stackoverflow.com/questions/40908949/asp-net-core-cors-webapi-no-access-control-allow-origin-header
-            app.UseCors(builder =>
-            builder
+            app.UseCors(builder => builder
                 .AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod());
@@ -64,6 +76,11 @@ namespace redis_core_back
                     template: "{controller=Redis}");
             });
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
